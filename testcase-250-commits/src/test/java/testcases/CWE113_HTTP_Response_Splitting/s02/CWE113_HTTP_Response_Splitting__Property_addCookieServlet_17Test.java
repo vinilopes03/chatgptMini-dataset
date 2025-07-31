ad1ece5838b3efd5e6198c3b5e6e15af545dfa15
vlopes <vlopes@Viniciuss-MacBook-Pro.local>
2025-07-31 02:40:31 -0400

    Initial project setup

---
+++ testcase-250-commits/src/test/java/testcases/CWE113_HTTP_Response_Splitting/s02/CWE113_HTTP_Response_Splitting__Property_addCookieServlet_17Test.java
@@ -0,0 +1,35 @@
+package testcases.CWE113_HTTP_Response_Splitting.s02;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import org.mockito.ArgumentCaptor;
+import javax.servlet.http.*;
+
+public class CWE113_HTTP_Response_Splitting__Property_addCookieServlet_17Test {
+
+    @Test
+    void testCWE113_HTTPResponseSplitting() throws Exception {
+        String maliciousInput = "en-US%0d%0aSet-Cookie:evil=1";
+        System.setProperty("user.home", maliciousInput); // Set the system property to the malicious input
+        CWE113_HTTP_Response_Splitting__Property_addCookieServlet_17 instance = new CWE113_HTTP_Response_Splitting__Property_addCookieServlet_17();
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        HttpServletRequest request = mock(HttpServletRequest.class);
+
+        try {
+            instance.bad(request, response);
+        } catch (Throwable t) {
+            fail("Vulnerability fixed or not triggered: Exception occurred");
+        }
+
+        ArgumentCaptor<Cookie> cookieCaptor = ArgumentCaptor.forClass(Cookie.class);
+        verify(response).addCookie(cookieCaptor.capture());
+        Cookie added = cookieCaptor.getValue();
+
+        boolean isVulnerable = added.getValue().contains("\r") || 
+                             added.getValue().contains("\n") || 
+                             added.getValue().contains("%0d%0a");
+        assertTrue(isVulnerable, 
+            "Vulnerability fixed or input sanitized: CRLF not propagated to cookie value");
+    }
+}
